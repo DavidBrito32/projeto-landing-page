ce2bc44381ae8a22eb02bd71d3e559c68e28ba95
DavidBrito32 <davidbrito.carneiro458@gmail.com>
2023-07-11 14:20:41 -0300

    adicionado tit, e pagina de acesso a conta

--- script/index.js
+++ script/index.js
@@ -4,4 +4,20 @@ abreMenu.addEventListener('click', () => {
     const navbar = document.querySelector('#navbar');
     navbar.classList.toggle('active');
     abreMenu.classList.toggle('ativo');
+});
+
+abreConta.addEventListener('click', () => {
+    overlay.classList.add('active');
+    conta.classList.add('active');
+})
+
+
+fechaConta.addEventListener('click', () => {
+    overlay.classList.remove('active');
+    conta.classList.remove('active');
+});
+
+overlay.addEventListener('click', () => {
+    overlay.classList.remove('active');
+    conta.classList.remove('active');
 })
